d6b5b77866e853fffa20ec5b9e4cd6fcbebed88f
an4nyaban <ananyaban06@gmail.com>
2025-02-08 18:30:53 -0500

    fixed music notes

--- EnemyNote.js
+++ EnemyNote.js
@@ -1,27 +1,22 @@
 /** @type {HTMLCanvasElement} */
 export default class EnemyNote {
-    constructor(ctx, speed) {
-        let x = 810;
-        let y = Math.random() * 600;
+    constructor(ctx, speed, y, letter) {
+        this.x = 810;
+        this.y = y;
         this.width = 80;
         this.height = 150;
         this.ctx = ctx;
-        this.letter = this.getRandomLetter();
+        this.letter = letter;
         this.speed = speed;
         this.image = new Image();
         this.image.src = "chordWithoutTail.png";
     }
 
-    getRandomLetter() {
-        const alphabet = "abcdefghijklmnopqrstuvwxyz";
-        const randomIndex = Math.floor(Math.random() * alphabet.length);
-        return alphabet[randomIndex];
-    }
-
     draw() {
         this.ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
         this.ctx.font = "30px solid";
-        this.ctx.fillText(this.letter, this.x, this.y);
+        this.ctx.fillStyle = "white";
+        this.ctx.fillText(this.letter, this.x + 26, this.y + 122);
     }
 
     update() {
