17b74bde0d06294b422959984828e1e19eb808ac
JaydenMBurrows <jaydenborrows@gmail.com>
2025-02-08 11:18:55 -0500

    instruments added to bottom

--- script.js
+++ script.js
@@ -2,10 +2,19 @@
 // this canvas hidden
 /** @type {HTMLCanvasElement} */
 const canvas = document.getElementById('canvas1');
-canvas.getContext('2d');
+const ctx = canvas.getContext('2d');
+const CANVAS_WIDTH = canvas.width = 600;
+const CANVAS_HEIGHT = canvas.height = 650; 
 
 // initialize the instrument images and their starting locations
-
+ctx.font = "80px Georgia";
+ctx.fillStyle = '#DAA06D';
+ctx.fillRect(0, CANVAS_HEIGHT - 120, CANVAS_WIDTH, 120);
+ctx.fillText('🎸', 0, CANVAS_HEIGHT - 30, 180);
+ctx.fillText('🥁', 120, CANVAS_HEIGHT - 30, 180);
+ctx.fillText('🎹', 240, CANVAS_HEIGHT - 30, 180);
+ctx.fillText('🎤', 360, CANVAS_HEIGHT - 30, 180);
+ctx.fillText('🎻', 480, CANVAS_HEIGHT - 30, 180);
 // music note class
 // music note generation - possibly different js script file 
 
